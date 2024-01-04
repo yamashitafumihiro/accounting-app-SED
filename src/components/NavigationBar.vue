b2d914b49f0331c8d0eb44d5de794e643e504d74
fumi <fumiwata2000@gmail.com>
2024-01-04 17:39:13 +0900

    土台の作成

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navigation-bar">
+      <ul>
+        <li><router-link to="/">ホーム</router-link></li>
+        <li><router-link to="/about">アバウト</router-link></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavigationBar'
+  };
+  </script>
+  
+  <style>
+  .navigation-bar ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .navigation-bar li {
+    display: inline;
+    margin-right: 20px;
+  }
+  </style>
+  
